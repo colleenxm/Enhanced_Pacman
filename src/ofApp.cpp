6362b1118ebe3ee3320ddd4540fcfee6113158fb
Elizabeth Wang <eywang1999@gmail.com>
2018-04-12 18:36:27 -0500

    added webcam

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,30 +6,34 @@ void ofApp::setup(){
     
     // note: not creating a panel for the introduction b/c I don't like how labels look and because it's really simple
     setupWebcamPanel();
+    webcam.setup(2000, 2000); // set up the webcam
 }
-//--------------------------------------------------------------
 
+//--------------------------------------------------------------
 void ofApp::setupWebcamPanel() { // sets up the intro panel
     photo_taking_button_.addListener(this,&ofApp::photoButtonPressed);
 
     webcam_panel_.setup("webcam panel");
     webcam_panel_.add(photo_taking_button_.setup("TAKE PHOTO"));
 }
-//--------------------------------------------------------------
 
-void ofApp::photoButtonPressed() {
-    ofxLabel p;
-    webcam_panel_.add(p.setup("button pressed", "")); // is working
+//--------------------------------------------------------------
+void ofApp::photoButtonPressed() { // listener for button - know that this is working
+    ofxButton p;
+    ofxPanel test_panel;
+    test_panel.setup();
+    test_panel.add(p.setup("BUTTON WAS PRESSED"));
+    test_panel.draw();
 }
-//--------------------------------------------------------------
 
+//--------------------------------------------------------------
 void ofApp::exit(){
     photo_taking_button_.removeListener(this, &ofApp::photoButtonPressed);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    
+    webcam.update();
 }
 
 //--------------------------------------------------------------
@@ -40,6 +44,7 @@ void ofApp::draw(){
     } else {
         ofClear(0);
         webcam_panel_.draw();
+        webcam.draw(ofGetWidth() - ofGetWidth()/1.1, ofGetHeight() - ofGetHeight()/1.1, ofGetWidth()/1.1, ofGetHeight()/1.1);
     }
 }
 
@@ -100,5 +105,3 @@ void ofApp::gotMessage(ofMessage msg){
 void ofApp::dragEvent(ofDragInfo dragInfo){
     
 }
-
-
--- src/ofApp.h
+++ src/ofApp.h
@@ -14,6 +14,7 @@ private:
     ofxButton photo_taking_button_;
     void photoButtonPressed(); // listener
     
+    ofVideoGrabber webcam;
 public:
     void setup();
     void update();
