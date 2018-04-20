67e5c908efd39edad19f9453ef4bef147d639f22
Elizabeth Wang <eywang1999@gmail.com>
2018-04-19 19:40:00 -0500

    Modified OfSnake code to not use LinkedLists - aka snake = 1 element

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,19 +3,12 @@
 #include <algorithm>
 #include <iostream>
 
+using namespace snakelinkedlist;
+
 // Setup method
-void EnhancedPacman::setup(){
+void snakeGame::setup(){
     ofSetWindowTitle("ENHANCED PACMAN");
     
-    string_font_.load("/Users/elizabeth/Downloads/of_v0.9.8_osx_release/examples/addons/networkTcpServerExample/bin/data/type/verdana.ttf", 32, true, false, true, 0.1);
-    
-    intro_music_.load("/Users/elizabeth/CS126-FINAL-PROJECT/final-project-ElizWang/sound_files/intro_music.mp3"); // fix this later?
-    
-    if (current_state_ == NOT_STARTED) { // only plays the intro music when the intro is being shown
-        intro_music_.setLoop(true); // plays over and over again
-        intro_music_.play();
-    }
-     
     srand(static_cast<unsigned>(time(0))); // Seed random with current time
 }
 
@@ -29,10 +22,22 @@ void EnhancedPacman::setup(){
  3. Update the snake in the current direction it is moving
  4. Check to see if the snakes new position has resulted in its death and the end of the game
  */
-void EnhancedPacman::update() {
+void snakeGame::update() {
     if (should_update_) {
         if (current_state_ == IN_PROGRESS) {
+            ofVec2f snake_body_size = game_snake_.getBodySize();
+            ofVec2f head_pos = game_snake_.getHead()->position;
+            ofRectangle snake_rect(head_pos.x, head_pos.y, snake_body_size.x, snake_body_size.y);
             
+            if (snake_rect.intersects(game_food_.getFoodRect())) {
+                game_snake_.eatFood(game_food_.getColor());
+                game_food_.rebase();
+            }
+            game_snake_.update();
+            
+            if (game_snake_.isDead()) {
+                current_state_ = FINISHED;
+            }
         }
     }
     should_update_ = true;
@@ -44,23 +49,15 @@ void EnhancedPacman::update() {
  2. If the game is finished draw the game over screen and final score
  3. Draw the current position of the food and of the snake
  */
-void EnhancedPacman::draw(){ // is called over and over again
-    if (current_state_ == NOT_STARTED) {
-        ofSetBackgroundColor(0, 0, 0); // black
-        ofSetColor(0, 90, 60); // dark green
-        ofFill();
-        
-        ofDrawBitmapString("Click anywhere to continue. ", ofGetWidth()/6, ofGetHeight()/6);
-        string_font_.drawString("WELCOME TO THE PACMAN GAME", ofGetWidth()/6, ofGetWidth()/6);
-  
-    } else if (current_state_ == IN_PROGRESS){
-        drawFood();
-        
-    } else if(current_state_ == PAUSED) {
+void snakeGame::draw(){ // is called over and over again
+    if(current_state_ == PAUSED) {
         drawGamePaused();
-    } else if(current_state_ == FINISHED) {
+    }
+    else if(current_state_ == FINISHED) {
         drawGameOver();
     }
+    drawFood();
+    drawSnake();
 }
 
 /*
@@ -76,7 +73,7 @@ void EnhancedPacman::draw(){ // is called over and over again
  and current_direction is not opposite of dir (Prevents the snake turning and eating itself)
  Update direction of snake and force a game update (see ofApp.h for why)
  */
-void EnhancedPacman::keyPressed(int key){
+void snakeGame::keyPressed(int key){
     if (key == OF_KEY_F12) {
         ofToggleFullscreen();
         return;
@@ -90,77 +87,74 @@ void EnhancedPacman::keyPressed(int key){
     }
     else if (current_state_ == IN_PROGRESS)
     {
+        SnakeDirection current_direction = game_snake_.getDirection();
+        
+        // If current direction has changed to a valid new one, force an immediate update and skip the next frame update
+        if (upper_key == 'W' && current_direction != DOWN && current_direction != UP) {
+            game_snake_.setDirection(UP);
+            update();
+            should_update_ = false;
+        }
+        
+        else if (upper_key == 'A' && current_direction != RIGHT && current_direction != LEFT) {
+            game_snake_.setDirection(LEFT);
+            update();
+            should_update_ = false;
+        }
+        
+        else if ((upper_key == 'S') && current_direction != UP && current_direction != DOWN) {
+            game_snake_.setDirection(DOWN);
+            update();
+            should_update_ = false;
+        }
+        else if (upper_key == 'D' && current_direction != LEFT && current_direction != RIGHT) {
+            game_snake_.setDirection(RIGHT);
+            update();
+            should_update_ = false;
+
+        } else if (upper_key == 'H') { // pauses the game and prints the highest scores
+            current_state_ = PAUSED;
+        }
     } else if (upper_key == 'R' && current_state_ == FINISHED) {
+        //updateHighestScores(game_snake_.getFoodEaten()); // "works" if updating here, problem is that I'll be one game behind
         reset();
     }
 }
 
-void EnhancedPacman::reset() {
-    // create more food objects (??)
+void snakeGame::reset() {
+    game_snake_ = Snake();
+    game_food_.rebase();
     current_state_ = IN_PROGRESS;
 }
 
-void EnhancedPacman::windowResized(int w, int h){
+void snakeGame::windowResized(int w, int h){
     game_food_.resize(w, h);
+    game_snake_.resize(w, h);
 }
 
-void EnhancedPacman::drawFood() {
+void snakeGame::drawFood() {
     ofSetColor(game_food_.getColor());
     ofDrawRectangle(game_food_.getFoodRect());
 }
 
-void EnhancedPacman::drawGameOver() {
-}
-
-void EnhancedPacman::drawGamePaused() {
-    string pause_message = "P to Unpause!";
-    ofSetColor(0, 0, 0);
-    ofDrawBitmapString(pause_message, ofGetWindowWidth() / 1.5, ofGetWindowHeight() / 1.5);
-}
-
-//--------------------------------------------------------------
-void EnhancedPacman::keyReleased(int key){
+void snakeGame::drawSnake() {
+    ofVec2f snake_body_size = game_snake_.getBodySize();
+    ofVec2f head_pos = game_snake_.getHead()->position;
+    ofSetColor(game_snake_.getHead()->color);
+    ofDrawRectangle(head_pos.x, head_pos.y, snake_body_size.x, snake_body_size.y);
     
-}
-
-//--------------------------------------------------------------
-void EnhancedPacman::mouseMoved(int x, int y ){
-    
-}
-
-//--------------------------------------------------------------
-void EnhancedPacman::mouseDragged(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void EnhancedPacman::mousePressed(int x, int y, int button){
-    if (current_state_ == NOT_STARTED) { // stop showing intro after the mouse is clicked
-        current_state_ = IN_PROGRESS; // start the game
+    for (SnakeBody* curr = game_snake_.getHead(); curr != NULL; curr = curr->next) {
+        ofVec2f currPos = curr->position;
+        ofSetColor(curr->color);
+        ofDrawRectangle(currPos.x, currPos.y, snake_body_size.x, snake_body_size.y);
     }
 }
 
-//--------------------------------------------------------------
-void EnhancedPacman::mouseReleased(int x, int y, int button){
-    
+void snakeGame::drawGameOver() {
 }
 
-//--------------------------------------------------------------
-void EnhancedPacman::mouseEntered(int x, int y){
-    
-}
-
-//--------------------------------------------------------------
-void EnhancedPacman::mouseExited(int x, int y){
-    
-}
-
-//--------------------------------------------------------------
-void EnhancedPacman::gotMessage(ofMessage msg){
-    
-}
-
-//--------------------------------------------------------------
-void EnhancedPacman::dragEvent(ofDragInfo dragInfo){
-    
+void snakeGame::drawGamePaused() {
+    string pause_message = "P to Unpause!";
+    ofSetColor(0, 0, 0);
+    ofDrawBitmapString(pause_message, ofGetWindowWidth() / 1.5, ofGetWindowHeight() / 1.5);
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -4,56 +4,49 @@
 #include <utility>
 
 #include "ofMain.h"
-#include "wanderer.hpp"
+#include "snake.h"
+#include "SnakeFood.h"
 
-// Enum to represent the current state of the game
-enum GameState {
-    NOT_STARTED = 0, // introduction
-    IN_PROGRESS,
-    PAUSED,
-    FINISHED
-};
-
-class EnhancedPacman : public ofBaseApp {
-private:
-    // FOR INTRODUCTION PANEL
-    ofSoundPlayer intro_music_; // plays music
-    ofTrueTypeFont string_font_; // sets font, used to print strings with colors/sizes/fonts other than the default
-
-    GameState current_state_ = NOT_STARTED; // should start at the introduction
-
-    Wanderer game_food_; // test wanderer
+namespace snakelinkedlist {
     
-    bool should_update_ = true; // derived from ofsnake MP
+    // Enum to represent the current state of the game
+    enum GameState {
+        IN_PROGRESS = 0,
+        PAUSED,
+        FINISHED
+    };
     
-    // Private helper methods to render various aspects of the game on screen.
-    void drawFood();
-    void drawGameOver();
-    void drawGamePaused();
-    
-    // Resets the game objects to their original state.
-    void reset();
-    
-public:
-    // Function used for one time setup
-    void setup();
-    
-    // Main event loop functions called on every frame
-    void update();
-    void draw();
-    
-    // Event driven functions, called on appropriate user action
-    void keyPressed(int key);
-    void windowResized(int w, int h);
-    
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void mouseEntered(int x, int y);
-    void mouseExited(int x, int y);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
-};
+    class snakeGame : public ofBaseApp {
+    private:
+        GameState current_state_ = IN_PROGRESS; // The current state of the game, used to determine possible actions
+        Snake game_snake_; // The object that represents the user controlled snake
+        SnakeFood game_food_; // The object that represents the food pellet the user is attempting to eat with the snake
+        
+        bool should_update_ = true;     // A flag boolean used in the update() function. Due to the frame dependent animation we've
+        // written, and the relatively low framerate, a bug exists where users can prefire direction
+        // changes faster than a frame update. Our solution is to force a call to update on direction
+        // changes and then not update on the next frame to prevent the snake from skipping across the screen.
+        
+        // Private helper methods to render various aspects of the game on screen.
+        void drawFood();
+        void drawSnake();
+        void drawGameOver();
+        void drawGamePaused();
+        
+        // Resets the game objects to their original state.
+        void reset();
+        
+    public:
+        // Function used for one time setup
+        void setup();
+        
+        // Main event loop functions called on every frame
+        void update();
+        void draw();
+        
+        // Event driven functions, called on appropriate user action
+        void keyPressed(int key);
+        void windowResized(int w, int h);
+    };
+} // namespace snakelinkedlist
 
